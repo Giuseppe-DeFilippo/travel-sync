aff64ee5c1b026cb870b34067d426b36b5aa212e
DoriaFabio <doriafabio2001@gmail.com>
2025-02-01 00:20:50 +0100

    do array data

---
+++ src/data/users.js
@@ -0,0 +1,49 @@
+const users = [
+    {
+        id: 1,
+        name: "Aladino",
+        surname: "Genovesi",
+        phone: "0394 1832625",
+        email: "aladinogenovesi@gmail.com",
+        date: "22/06/1978",
+        cf: "GNVLDN78H22G702Q"
+    },
+    {
+        id: 2,
+        name: "Dina",
+        surname: "Ricci",
+        phone: "0399 1708833",
+        email: "dinaricci@gmail.com",
+        date: "27/11/1971",
+        cf: "RCCDNI71S62G273I"
+    },
+    {
+        id: 3,
+        name: "Franco",
+        surname: "Colombo",
+        phone: "0379 9894757",
+        email: "francocolombo@gmail.com",
+        date: "23/04/1991",
+        cf: "CLMFNC91D23C351J"
+    },
+    {
+        id: 4,
+        name: "Alberta",
+        surname: "Cocci",
+        phone: "0364 6426471",
+        email: "albertacocci@gmail.com",
+        date: "08/07/2005",
+        cf: "CCCLRT05L08B519L"
+    },
+    {
+        id: 5,
+        name: "Giacinto",
+        surname: "Calabresi",
+        phone: "0349 2848381",
+        email: "giacintocalabresi@gmail.com",
+        date: "10/11/1961",
+        cf: "CLBGNT61S10D086O"
+    },
+];
+
+export default users;
