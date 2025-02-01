df9d2a0b32de064a070b86631b404c286ac740c0
giuse <giuseppedefilippo03@gmail.com>
2025-02-01 02:23:15 +0100

    css aggiunto

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -12,8 +12,8 @@ export default function CardComponent({ dataCards }) {
 
     return (
         <Link to={getRoute(dataCards.description)}>
-            <div div className="card col-sm-6 col-md-6 p-0 cover rounded-4 " style={{ width: "12rem" }}>
-                <img src={dataCards.img} className="rounded-4" style={{ width: "12rem", height: "12rem" }} alt={dataCards.description} />
+            <div div className="card h-100  " style={{ width: "12rem" }}>
+                <img src={dataCards.img} className="card-img-top" style={{ width: "12rem", height: "12rem" }} alt={dataCards.description} />
                 <div className="card-body d-none">
                     <p className="card-text ">{dataCards.description}</p>
                 </div>
